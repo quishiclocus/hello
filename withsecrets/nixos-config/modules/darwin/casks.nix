f64662ded2b6c31e298591ab061cf0b16b3253d0
Chuck Stearns <chuck.stearns@gmail.com>
2024-07-31 11:35:50 -0500

    nixos-config with secrets

---
+++ withsecrets/nixos-config/modules/darwin/casks.nix
@@ -0,0 +1,27 @@
+_:
+
+[
+  # Development Tools
+#  "homebrew/cask/docker"
+  "homebrew/cask/rancher"
+  "visual-studio-code"
+
+  # Communication Tools
+#  "discord"
+  "notion"
+  "slack"
+#  "telegram"
+#  "zoom"
+
+  # Utility Tools
+#  "syncthing"
+
+  # Entertainment Tools
+#  "vlc"
+
+  # Productivity Tools
+#  "raycast"
+
+  # Browsers
+  "google-chrome"
+]
---
+++ withsecrets/nixos-config/modules/shared/packages.nix
@@ -0,0 +1,64 @@
+{ pkgs }:
+
+with pkgs; [
+  # General packages for development and system management
+  alacritty
+  aspell
+  aspellDicts.en
+  bash-completion
+  bash
+  bat
+  btop
+  coreutils
+  wget
+  zip
+  direnv
+  go
+  gh
+
+  # Encryption and security tools
+  age
+  #age-plugin-yubikey
+  #gnupg
+  #libfido2
+
+  # Cloud-related tools and SDKs
+  #docker
+  #docker-compose
+  rancher
+
+  # Media-related packages
+  #emacs-all-the-icons-fonts
+  dejavu_fonts
+  ffmpeg
+  fd
+  font-awesome
+  feather-font # from overlay
+  hack-font
+  noto-fonts
+  noto-fonts-emoji
+  meslo-lgs-nf
+
+  # Node.js development tools
+  #nodePackages.npm # globally install npm
+  #nodePackages.prettier
+  #nodejs
+
+  # Text and terminal utilities
+  htop
+  #hunspell
+  iftop
+  #jetbrains-mono
+  jq
+  ripgrep
+  tree
+  tmux
+  unrar
+  unzip
+  zsh-powerlevel10k
+
+  # Python packages
+  (python3.withPackages (ps: [
+    ps.flask
+  ]))
+]
